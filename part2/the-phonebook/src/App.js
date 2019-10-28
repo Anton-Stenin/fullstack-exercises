83ce745455996fb89c3a28f0166444c0ce0270ee
Anton <54212748+Anton-Stenin@users.noreply.github.com>
2019-10-28 18:56:16 +0300

    2.20*: Phonebook step12

--- part2/the-phonebook/src/App.js
+++ part2/the-phonebook/src/App.js
@@ -2,13 +2,34 @@ import React, { useState, useEffect } from 'react'
 import Filter from './components/Filter'
 import Persons from './components/Persons'
 import PersonForm from './components/PersonForm'
+import Notification from './components/Notification'
 import personService from './services/persons'
+
+const messageStyle = {
+  background: 'lightgrey',
+  fontSize: 20,
+  borderStyle: 'solid',
+  borderRadius: 5,
+  padding: 10,
+  marginBottom: 10,
+}
+const normalMessageStyle = {
+  ...messageStyle,
+  color: 'green'
+}
+
+const errorMessageStyle = {
+  ...messageStyle,
+  color: 'red'
+}
 
 const App = () => {
   const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [filter, setFilter] = useState('')
+  const [message, setMessage] = useState(null)
+  const [messageStyle, setMessageStyle] = useState()
 
   useEffect(() => {
     // console.log('effect')
@@ -32,53 +53,70 @@ const App = () => {
     setNewNumber(event.target.value)
   }
 
+  function resetInputAndMessage() {
+    setNewNumber('')
+    setNewName('')
+    setTimeout(() => setMessage(null), 5000)
+  }
+
   const personsToShow =
     (filter === '')
       ? persons
       : persons.filter(person => person.name.includes(filter))
 
-  const updatePersonsNumber = (personToUpdate) => {
+  const updatePersonsNumber = () => {
+    const personToUpdate = persons.find(person => person.name === newName)
+    console.log(`person to update:`, personToUpdate)
     const id = personToUpdate.id
     const changedPerson = { ...personToUpdate, number: newNumber }
     console.log(`changed person:`, changedPerson)
 
     personService
       .update(id, changedPerson)
-      .then(returnedPerson => setPersons(
-        persons.map(
-          person =>
-            {
-              console.log(`the person mapped is now:`, person)
-              return person.id !== id ? person : returnedPerson;
-            }
-        )
+      .then(returnedPerson => {
+        setPersons(
+          persons.map(person => person.id !== id ? person : returnedPerson));
+        setMessageStyle(normalMessageStyle)
+        setMessage(`${returnedPerson.name} phone number is changed`)
+        resetInputAndMessage()
+      }
       )
+      .catch(
+        error => {
+          setPersons(persons.filter(person => person.id !== id))
+          setMessageStyle(errorMessageStyle)
+          setMessage(`Information on ${personToUpdate.name} has already been removed from the server`)
+          resetInputAndMessage()
+        }
       )
-    setNewNumber('')
-    setNewName('')
+  }
+
+  const createNewPerson = () => {
+    const personObject = {
+      name: newName,
+      number: newNumber
+    }
+    personService
+      .create(personObject)
+      .then(
+        newPerson => {
+          setPersons(persons.concat(newPerson))
+          setMessageStyle(normalMessageStyle)
+          setMessage(`Added ${newPerson.name}`)
+          resetInputAndMessage()
+        }
+      )
   }
 
   const addPerson = (event) => {
     event.preventDefault();
     if (personsToShow.map(person => person.name).includes(newName)) {
       if (window.confirm(`${newName} is already added to the phonebook, replace the old number with a new one?`)) {
-        const personToUpdate = persons.find(person => person.name === newName)
-        console.log(`person to update:`, personToUpdate)
-        updatePersonsNumber(personToUpdate)
+        updatePersonsNumber()
         return
       }
     }
-    const personObject = {
-      name: newName,
-      number: newNumber
-    }
-    personService.create(personObject).then(
-      newPerson => {
-        setPersons(persons.concat(newPerson))
-        setNewNumber('')
-        setNewName('')
-      }
-    )
+    createNewPerson()
   }
 
   const deletePerson = (personToDelete) => {
@@ -87,12 +125,15 @@ const App = () => {
       console.log(`person with id ${personToDelete.id} is deleted`)
       personService.deletePerson(personToDelete.id)
       setPersons(persons.filter(person => person.id !== personToDelete.id))
+      setMessageStyle(normalMessageStyle)
+      setMessage(`${personToDelete.name} is deleted`)
     }
   }
 
   return (
     <div>
       <h2>Phonebook</h2>
+      <Notification message={message} style={messageStyle} />
       <Filter value={filter} onChangeHandler={handleFilter} />
       <h3>Add a new</h3>
       <PersonForm
@@ -109,3 +150,5 @@ const App = () => {
 }
 
 export default App
+
+
